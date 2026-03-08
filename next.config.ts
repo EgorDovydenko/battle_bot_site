8a91c158ef8ab533ff7fdaefaa6ae5921254a79e
YegorDovydenko <dovydenko.e93@gmail.com>
2026-03-08 16:26:58 +0300

    change images urls next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      new URL("https://battle-bot-site.onrender.com//**"),
+      new URL("https://chain-club.by/**"),
+    ],
+  },
+  devIndicators: false,
 };
 
 export default nextConfig;
