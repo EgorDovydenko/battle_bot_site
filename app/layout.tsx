0dcf6d7dea553a11b0a6c567753e82c5c26d20b4
YegorDovydenko <dovydenko.e93@gmail.com>
2026-03-08 16:06:49 +0300

    seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,31 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "БК \"ЦЕПЬ\"",
-  description: "Бойцовский клуб \"ЦЕПЬ\" в Беларуси. Тренировки и бои по всем видам единоборств.",
+  title: 'БК "ЦЕПЬ"',
+  description:
+    'Бойцовский клуб "ЦЕПЬ" в Беларуси. Тренировки и бои по всем видам единоборств.',
+  icons: {
+    icon: [
+      {
+        url: "/icons/favicons/icon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        url: "/icons/favicons/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+    ],
+    apple: [
+      {
+        url: "/icons/favicons/icon-192x192.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  },
+  metadataBase: "https://chain-club.by",
 };
 
 export default function RootLayout({
