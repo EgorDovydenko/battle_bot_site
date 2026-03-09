74016d6008d6b284e2466de59138b4b157a4db68
YegorDovydenko <dovydenko.e93@gmail.com>
2026-03-09 13:21:37 +0300

    new form link

--- shared/constants/index.tsx
+++ shared/constants/index.tsx
@@ -8,11 +8,16 @@ export const links = {
   telegram: "https://t.me/battle_bot_test_bot",
   instagram: "https://instagram.com/",
   tiktok: "https://tiktok.com/",
-  terms: "https://example.com/terms",
-  fightersRequestForm: "https://docs.google.com/forms/d/e/1FAIpQLSfNnvZ6QITUELJhQomBjTqDM6QFapLEn15xSn09wQ9C8H6Ayw/viewform?usp=header",
-  fightersTrainingForm: "https://docs.google.com/forms/d/e/1FAIpQLSfxtT6aBPiOiDwhBuYpKNWqJlAXfVgW00VFFi3-UA8db1xV0Q/viewform?usp=header",
-  viewersTicketsForm: "https://docs.google.com/forms/d/e/1FAIpQLSc3JyFDHVchDrp2KdfjxzKRsv1VKfqqihn0Pa1ptt1Zxl7SUg/viewform?usp=header",
+  terms:
+    "https://docs.google.com/document/d/1o0G1Uh4dZu0OXwVGYVJG9SXxvnaqRLzOkBnS5h5KM4Y/edit?usp=sharing",
+  fightersRequestForm:
+    "https://docs.google.com/forms/d/e/1FAIpQLSfNnvZ6QITUELJhQomBjTqDM6QFapLEn15xSn09wQ9C8H6Ayw/viewform?usp=header",
+  fightersTrainingForm:
+    "https://docs.google.com/forms/d/e/1FAIpQLSfxtT6aBPiOiDwhBuYpKNWqJlAXfVgW00VFFi3-UA8db1xV0Q/viewform?usp=header",
+  viewersTicketsForm:
+    "https://docs.google.com/forms/d/e/1FAIpQLSc3JyFDHVchDrp2KdfjxzKRsv1VKfqqihn0Pa1ptt1Zxl7SUg/viewform?usp=header",
   donationsLinkOne: "https://example.com/donate1",
   donationsLinkTwo: "https://example.com/donate2",
-  donationsForm: "https://docs.google.com/forms/d/e/1FAIpQLSdMMYL5-_B4-RqQsbwNE4_gjMmDXhXVfbMEnpDekanJrZNMfw/viewform?usp=header",
+  donationsForm:
+    "https://docs.google.com/forms/d/e/1FAIpQLSdMMYL5-_B4-RqQsbwNE4_gjMmDXhXVfbMEnpDekanJrZNMfw/viewform?usp=header",
 };
